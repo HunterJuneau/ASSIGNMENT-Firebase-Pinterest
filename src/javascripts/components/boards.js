2b738745aaacfe477dce2ee22dcf34ad466faf06
HunterJuneau <huntlee17@gmail.com>
2021-03-04 19:57:14 -0600

    Added navbar with login and logout buttons.

---
+++ src/javascripts/components/boards.js
@@ -0,0 +1,5 @@
+const boards = () => {
+  $('#page').html('<h1>Boards</h1>');
+};
+
+export default boards;
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,12 @@
-// USE WITH FIREBASE AUTH
-// import checkLoginStatus from './helpers/auth';
+import navbar from './components/navbar';
+import checkLoginStatus from './helpers/auth';
 import 'bootstrap'; // import bootstrap elements and js
 
 import '../styles/main.scss';
 
 const init = () => {
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+  navbar();
+  checkLoginStatus();
 };
 
 init();
